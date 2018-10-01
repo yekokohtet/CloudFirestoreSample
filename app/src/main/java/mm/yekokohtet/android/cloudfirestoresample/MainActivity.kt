51fe652fb7837c006ab01ee95c78237ca4706674
YeKoKoHtetMMDS <yekokohtet@mm-digital-solutions.com>
2018-10-01 14:15:05 +0630

    Initial commit

---
+++ app/src/main/java/mm/yekokohtet/android/cloudfirestoresample/MainActivity.kt
@@ -0,0 +1,12 @@
+package mm.yekokohtet.android.cloudfirestoresample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
